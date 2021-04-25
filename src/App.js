5acd1e020651e90c19e856a588ef77c159581cd3
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-04-24 23:07:39 -0300

    feat: creating Aleatorio component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 // import Primeiro from './components/Primeiro'
 // import { Comp, Comp1, Comp2 } from './components/Multi'
-import MinMax from './components/MinMax'
+// import MinMax from './components/MinMax'
+import Aleatorio from './components/Aleatorio'
 
 export default () => (
     <View style={style.App}>
-        <MinMax min="3" max="20"/>
+        <Aleatorio min={1} max={60}/>
+        {/*<MinMax min="3" max="20"/>*/}
         {/*<Primeiro/>*/}
         {/*<Comp/>*/}
         {/*<Comp1/>*/}
