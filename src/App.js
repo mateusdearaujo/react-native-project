407e14b9995b4a6a5019921cff263b03216ec6d2
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-04-24 23:29:14 -0300

    feat: creating Titulo component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { View, StyleSheet } from 'react-native'
 // import Primeiro from './components/Primeiro'
 // import { Comp, Comp1, Comp2 } from './components/Multi'
 // import MinMax from './components/MinMax'
-import Aleatorio from './components/Aleatorio'
+// import Aleatorio from './components/Aleatorio'
+import Titulo from './components/Titulo'
 
 export default () => (
     <View style={style.App}>
-        <Aleatorio min={1} max={60}/>
+        <Titulo principal="Cadastro Produto" secundario="Tela de Cadastro do Produto"/>
+        {/*<Aleatorio min={1} max={60}/>*/}
         {/*<MinMax min="3" max="20"/>*/}
         {/*<Primeiro/>*/}
         {/*<Comp/>*/}
