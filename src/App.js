04c2fe23bcfc5c7c99487031bcbef8e982cff0eb
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-04-24 22:14:54 -0300

    feat: creating multi component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import Primeiro from './components/Primeiro'
+import { Comp, Comp1, Comp2 } from './components/Multi'
 
 export default () => (
     <View>
-        <Primeiro />
+        <Primeiro/>
+        <Comp/>
+        <Comp1/>
+        <Comp2/>
     </View>
 )
---
+++ src/components/Multi.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Comp = () => {
+    return <Text>Comp #Oficial</Text>
+}
+
+const Comp1 = () => {
+    return <Text>Comp #01</Text>
+}
+
+const Comp2 = () => {
+    return <Text>Comp #02</Text>
+}
+
+export {
+    Comp,
+    Comp1,
+    Comp2
+}
