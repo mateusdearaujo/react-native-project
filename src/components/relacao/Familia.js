a2d5ab19fd31c3a3dd3380788b38f41ba904622a
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-05-20 21:52:57 -0300

    feat: creating Membro component

---
+++ src/components/relacao/Familia.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Filho from './Filho'
+import { Text } from 'react-native'
+import Estilo from '../estilo'
+
+export default () => {
+    return (
+        <>
+            <Filho nome="Anna" sobrenome="Silva"/>
+            <Filho nome="Julia" sobrenome="Silva"/>
+        </>
+    )
+}
